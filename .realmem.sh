f9e508ecea8b196c9762ec1bae049f162570e1e3
Ivan "Colona" Delalande <colona@ycc.fr>
2014-07-02 01:23:19 +0200

    Change RAM usage stat script to work with dash.

--- .realmem.sh
+++ .realmem.sh
@@ -3,5 +3,5 @@ mem=$(free -m | head -2 | tail -1)
 totmem=$(echo $mem | cut -f 2 -d ' ')
 realmem=$(( $(echo $mem | cut -f 3 -d ' ') - $(echo $mem | cut -f 6 -d ' ') - $(echo $mem | cut -f 7 -d ' ') ))
 percmem="$(($realmem * 100 / $totmem))%"
-realmem+="M "
+realmem="${realmem}M"
 echo "$realmem $percmem"
