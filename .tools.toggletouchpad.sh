49874ce0599c2508117dfc8b80f9053a9041b826
Ivan Delalande <colona@ycc.fr>
2017-08-04 03:35:11 -0700

    minor dwm, tmux and tools updates

--- .tools.toggletouchpad.sh
+++ .tools.toggletouchpad.sh
@@ -3,8 +3,8 @@
 if synclient > /dev/null 2>&1; then
 	current="$(synclient | grep TouchpadOff | cut -d '=' -f 2)"
 	synclient TouchpadOff="$((1 - $current))"
-elif xinput | grep -q 'Synaptics TouchPad'; then
-	devid="$(xinput | sed -rn '/Synaptics TouchPad/s/.*id=([0-9]+).*/\1/gp')"
+elif xinput | grep -q 'Synaptics '; then
+	devid="$(xinput | sed -rn '/Synaptics /s/.*id=([0-9]+).*/\1/gp')"
 	current="$(xinput list-props $devid | sed -rn '/Device Enabled/s/.*\s+([10])$/\1/gp')"
 	if [ "$current" -eq 0 ]; then
 		xinput enable "$devid"
