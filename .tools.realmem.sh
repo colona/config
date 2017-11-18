444d532a74bd6374da40c1edcfa14b34a55d489d
Ivan Delalande <colona@ycc.fr>
2017-11-17 21:23:13 -0800

    tools.sh: bring realmem 10 years into the future of 2014
    Per 34e431b0ae39 ("/proc/meminfo: provide estimated available memory").

--- .tools.realmem.sh
+++ .tools.realmem.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 exec < /proc/meminfo
 read __ totmem __
-read __ freemem __
 read __
-read __ buffmem __
-read __ cachemem __
-realmem=$((totmem - freemem - buffmem - cachemem))
+read __ availmem __
+realmem=$((totmem - availmem))
 percmem="$((realmem * 100 / totmem))%"
 realmem="$((realmem / 1024))M"
 echo "$realmem $percmem"
